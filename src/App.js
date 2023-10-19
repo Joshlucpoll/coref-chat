a565686c952e89b2620ee670d17f7d0901c8099a
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2023-10-19 00:22:43 +0000

    added alt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const style = { darkMode: true, accentColor: "#123456" }
   return (
     <div className="App">
-      <img id='logo' src={Logo}></img>
+      <img id='logo' src={Logo} alt='logo'></img>
       <p>Discover COREF Insights: Engage in Conversations with our Knowledgeable AI Chatbot</p>
       <div class='wrapper'>
         <MendableInPlace anon_key='3b80e6f0-aa2e-4b41-8835-02d9c4050528' style={style} hintText="Ask about COREF..." />
