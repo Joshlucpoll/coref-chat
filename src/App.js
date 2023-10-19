dd5e6a28c5af70fb16d2817c4a3bbd16796cd704
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2023-10-19 00:13:57 +0000

    test app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { MendableInPlace } from "@mendable/search"
+import Logo from './logo.svg'
+import './App.css';
+
+function App() {
+  const style = { darkMode: true, accentColor: "#123456" }
+  return (
+    <div className="App">
+      <img id='logo' src={Logo}></img>
+      <p>Discover COREF Insights: Engage in Conversations with our Knowledgeable AI Chatbot</p>
+      <div class='wrapper'>
+        <MendableInPlace anon_key='3b80e6f0-aa2e-4b41-8835-02d9c4050528' style={style} hintText="Ask about COREF..." />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
